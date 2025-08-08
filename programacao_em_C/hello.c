ea9d99e53b17dcc331d7b4dbf6ccda58ad8264ae
thiago <ts2383003@gmail.com>
2025-08-08 14:50:22 -0300

    treinamento

---
+++ programacao_em_C/hello.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+int main(){
+    int numero = 0;
+    printf("digite qualquer numero:\n");
+    scanf("%d", &numero);
+    printf("o numerodigitado foi%d \n", numero);
+    return 0;
+}
